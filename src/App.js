5fedd190427d2e978d14278f1938957dde06e20a
Dan Constantinescu <dan.constantinescu@softvision.ro>
2017-09-26 11:12:35 +0300

    finished app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-
 import './App.css';
+
 import BookList from './containers/BookList';
+import BookDetail from './containers/BookDetail';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<BookList />
+				<BookDetail />
 			</div>
 		);
 	}
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,4 @@
 export function selectBook(book){
-    console.log('a book', book.title);
     return {
         type: 'BOOK_SELECTED',
         payload: book
